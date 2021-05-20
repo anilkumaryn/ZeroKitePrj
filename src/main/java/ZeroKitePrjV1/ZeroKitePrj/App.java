1ef6e76c9eb04966d9f2a01ab4578bf5d447cc89
Anil Kumar Y N <43409974+anilkumaryn@users.noreply.github.com>
2021-05-20 09:00:36 +0530

    First commit

---
+++ src/main/java/ZeroKitePrjV1/ZeroKitePrj/App.java
@@ -0,0 +1,13 @@
+package ZeroKitePrjV1.ZeroKitePrj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
