c12a3e684a5ff079420b85501d75eaa1d488b1c3
Anil Kumar Y N <43409974+anilkumaryn@users.noreply.github.com>
2021-05-20 23:32:50 +0530

    updated with chromedriver

---
+++ src/test/java/com/KitePrj/BaseClass/BaseClass.java
@@ -0,0 +1,23 @@
+
+
+package com.KitePrj.BaseClass;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+
+	
+	public String  BaseUrl ="https://kite.zerodha.com/";
+	public String	UserName=""; 	
+	public String	Password="";
+	public String 	MPin ="";
+	public  WebDriver driver;
+	
+	public void setup() {
+		
+		
+		
+	}
+	
+	
+}
